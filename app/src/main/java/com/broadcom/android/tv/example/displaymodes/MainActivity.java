885f3647376cf1d4402ec6f67c923a29519cfdb9
Dominic Lemire <dominic.lemire@broadcom.com>
2018-12-17 13:44:57 -0800

    Initial commit

---
+++ app/src/main/java/com/broadcom/android/tv/example/displaymodes/MainActivity.java
@@ -0,0 +1,29 @@
+package com.broadcom.android.tv.example.displaymodes;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView mRecyclerView = findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        RecyclerView.Adapter adapter = new MyAdapter(this);
+        mRecyclerView.setAdapter(adapter);
+    }
+}
